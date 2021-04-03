a4e0e3fb05fa726d85b7b2c6f79e411368ab43ee
m-musha <gnmknr@gmail.com>
2021-04-03 07:45:03 +0000

    [ADD]comment_function

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books, only: [:edit, :create, :index, :show, :update, :destroy]
   resources :users, only: [:edit, :update, :index, :show]
-
+  resources :books, only: [:edit, :create, :index, :show, :update, :destroy] do
+   resources :book_comments, only: [:create, :destroy]
+   　resource :favorites, only: [:create, :destroy]
+ end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_054720) do
+ActiveRecord::Schema.define(version: 2021_04_02_052729) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.text "title"
