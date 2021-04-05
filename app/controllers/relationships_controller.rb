595d83efbfad5acd8c83307fd123c83e5147008f
m-musha <gnmknr@gmail.com>
2021-04-05 09:17:21 +0000

    test

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,12 +2,12 @@ class RelationshipsController < ApplicationController
 
   def follow
     current_user.follow(params[:id])
-    redirect_to users_path
+    redirect_to request.referer
   end
 
   def unfollow
     current_user.unfollow(params[:id])
-    redirect_to users_path
+    redirect_to request.referer
   end
 
 end
