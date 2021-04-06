f703fb4982b57bb57bd9b198b6bca034e24ed09e
m-musha <gnmknr@gmail.com>
2021-04-06 07:24:07 +0000

    検索機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,21 @@ class User < ApplicationRecord
    def following?(user)
      following_user.include?(user)
    end
+   
+    def self.search(search,word)
+     if search == "forward_match"
+        @user = User.where("name LIKE?","#{word}%")
+     elsif search == "backward_match"
+        @user = User.where("name LIKE?","%#{word}")
+     elsif search == "perfect_match"
+        @user = User.where(name: "#{word}")
+        
+     elsif search == "partial_match"
+        @user = User.where("name LIKE?","%#{word}%")
+     else
+        @user = User.all
+     end
+    end
 
    validates :name, uniqueness: true, length: {minimum: 2, maximum: 20}
    validates :introduction, length: {maximum: 50}
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  get 'search' => 'searches#search'
   resources :users, only: [:edit, :update, :index, :show] do
    member do 
     get 'following'
